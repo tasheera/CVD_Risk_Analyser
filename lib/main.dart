7df829cec02aa100c88cd73a5f8c06de3789cbcb
tasheera <131450540+tasheera@users.noreply.github.com>
2024-01-27 05:49:12 +0530

    add background image

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "CVD Risk Analyser",
+      home: ChartChoose(),
+    );
+  }
+}
+
+class ChartChoose extends StatelessWidget {
+  const ChartChoose({super.key});
+
+  @override
+  Widget build(BuildContext context) {// return scaffold
+    return Scaffold( 
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset("assets/chartChoose.png",fit: BoxFit.cover,),// Background Image
+        ],
+      ),
+    );
   }
 }
