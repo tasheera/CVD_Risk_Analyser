3dc7a63f8f478fbe28df7685957b7b911c81027c
tasheera <131450540+tasheera@users.noreply.github.com>
2024-02-03 01:26:10 +0530

    adjust responsiveness of screen

--- lib/doctor_data_input_screen.dart
+++ lib/doctor_data_input_screen.dart
@@ -11,27 +11,28 @@ class DoctorInputScreen extends StatefulWidget {
 class _DoctorInputScreenState extends State<DoctorInputScreen> {
   @override
   Widget build(BuildContext context) {
+    double iconSize = MediaQuery.of(context).size.width * 0.08; // return current screen width 
     return Scaffold(
       appBar: AppBar(
         title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween, // distribute free space
           children: [
             const Text('Date\nCheck CVD risk chart below', style: TextStyle(fontSize: 18),),
+            
             Image.asset(
               'assets/data_input_Vector_icon.png', //image file direct
-              width: 30, // height of image
-              height: 30, // height of image
-              ),
-            Image.asset(
+              width: iconSize*0.7,// adjust width of image using current screen width
+              height: iconSize*0.7, // height of image
+            ),
+              Image.asset(
               'assets/data_input_report_icon.png', //image file direct
-              width: 60, // width of image
-              height: 60, // height of image
-              ),
-        
-              
+              width: iconSize*1.6, // adjust width of image using current screen width
+              height: iconSize*1.6, // height of image
+            ),
+          
             ],
           ),
-        
+
       ),
     );
   }
