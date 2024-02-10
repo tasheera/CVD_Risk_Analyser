e2f1ae3be72c45ef74692e445216be8456c67e9a
tasheera <131450540+tasheera@users.noreply.github.com>
2024-02-10 08:48:02 +0530

    add controllers to text fields

--- lib/doctor_data_input_screen.dart
+++ lib/doctor_data_input_screen.dart
@@ -13,63 +13,56 @@ class _DoctorInputScreenState extends State<DoctorInputScreen> {
 
 final GlobalKey <FormState> _formKey =GlobalKey();// create global key object form key validate
 
-  String name="";
+  final TextEditingController _ageCVDController = TextEditingController(); // controller for age text field
+  final TextEditingController _genderCVDController = TextEditingController(); // controller for gender text field
+  final TextEditingController _heightCVDController = TextEditingController();// controller for height text field
+  final TextEditingController _weightCVDController = TextEditingController();// controller for weight field
 
-  Widget _buildNameField(String fieldName, validationFunction, error, keyboardType){
-    fieldName=fieldName;
-    error=error;
+// create function for return text filed with parameters
+  Widget _buildRiskInputField(String fieldName, validationFunction, error, keyboardType, controller){
+    fieldName=fieldName; // assign parameter to local variable
+    error=error; // assign parameter to local variable
     
     return Container(
-      
-      height: 50,
-      
-      decoration: BoxDecoration(
-        color: Colors.blue.withOpacity(0.09),
+      height: 50, // height of container
+      decoration: BoxDecoration( // add decoration to container
+        color: Colors.blue.withOpacity(0.09), // opacity gives transparency
         borderRadius: BorderRadius.circular(15),
-        border: Border.all(),
+        border: Border.all(), // add border
         boxShadow: [
           BoxShadow(
-            color: const Color.fromARGB(255, 48, 154, 240).withOpacity(0.11), // Shadow color
-            
-            
+            color: const Color.fromARGB(255, 48, 154, 240).withOpacity(0.11), //  color of the shadow
             offset: const Offset(0, 2), // Offset (vertical, horizontal)
           ),
         ],
-
       ),
-      child: Align(
-        alignment: Alignment.center,
-        child: TextFormField(
-          keyboardType: keyboardType,
-          decoration: InputDecoration(
-            
-            
-            
 
-            hintText: fieldName,
-                    border: const OutlineInputBorder(
-          borderSide: BorderSide.none,
-        ),
-            
-            
+      child: Align(
+        alignment: Alignment.center, // center the text in field
+        child: TextFormField(
+          controller: controller, // assign controller to text field
+          keyboardType: keyboardType, // assign key board type
+          decoration: InputDecoration(
+            hintText: fieldName, // hint text of the text field
+            border: const OutlineInputBorder(
+              borderSide: BorderSide.none, // remove selected effect in text field
+            ),
+
             hintStyle: const TextStyle(
-              color: Color(0xFF858EA9),
-              
-              
-              
+              color: Color(0xFF858EA9), // color of the hint text
             ),
-            contentPadding: const EdgeInsets.symmetric(horizontal: 60),
+
+            contentPadding: const EdgeInsets.symmetric(horizontal: 60), // add extra space before type location of the text field
             
             ),
             
-          validator: (value){
+          validator: (value){  // validate use input
             if (value == null || value.isEmpty){// check null or is empty
               return "Can not empty";
               }
-              if(!validationFunction(value)){
-                return error;
+              if(!validationFunction(value)){ // assign validate function
+                return error; // error message to show
               }
-              name=value;
               return null;          
           },
           ),
@@ -91,7 +84,7 @@ final GlobalKey <FormState> _formKey =GlobalKey();// create global key object fo
             const Text('Date\nCheck CVD risk chart below', style: TextStyle(fontSize: 18),),
             
             Image.asset(
-              'assets/data_input_Vector_icon.png', //image file direct
+              'assets/data_input_Vector_icon.png', //image file directory
               width: iconSize*0.7,// adjust width of image using current screen width
               height: iconSize*0.7, // height of image
             ),
@@ -106,21 +99,20 @@ final GlobalKey <FormState> _formKey =GlobalKey();// create global key object fo
 
       ),
 
-      body: SingleChildScrollView(
+      body: SingleChildScrollView( // enable scroll option
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+          // mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Center(
               child: Container(    
-                width: iconSize*11,
-                height: iconSize*4.5,
+                width: iconSize*11, //width of the container related to current screen
+                height: iconSize*4.5, //height of the container related to current screen
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(36),
                 color: const Color(0xFFD80032),
                 ),
                 
                 child: Row(
-                   //mainAxisAlignment: MainAxisAlignment., // distribute free space
                    children: [
                     
                     Image.asset( // doctor icon
@@ -129,8 +121,7 @@ final GlobalKey <FormState> _formKey =GlobalKey();// create global key object fo
                       height: iconSize*3.8, // height of image
                     ),
         
-        
-        
+
                     Text(
                       "Dr name\n position and hospital",
                       style: TextStyle(
@@ -154,10 +145,9 @@ final GlobalKey <FormState> _formKey =GlobalKey();// create global key object fo
                 ),
             ),
         
-        
-        
-        
-              Container(
+
+
+              Container( // used for show previous result
                 height: iconSize*3.2,
                 width: iconSize*6,
                 decoration: BoxDecoration(
@@ -179,54 +169,55 @@ final GlobalKey <FormState> _formKey =GlobalKey();// create global key object fo
         
         
               Form(
-            key: _formKey, // object is created above
-            child: Container(
-              margin: const EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,// left align text
-                children: [
-                  
-                  const Text("Age :",style: TextStyle(fontSize: 17,fontFamily: "Oswald"),),
-                  const SizedBox(height: 5,),
-                  _buildNameField("input age between 20 and 80", isValidAge,"Age should be between 20 and 80",TextInputType.number),
+                key: _formKey, // assign key of the foam widget which is created in above
+                child: Container(
+                  margin: const EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,// left align text
+                    children: [
 
-                  const SizedBox(height: 10,),
+                      const Text("Age :",style: TextStyle(fontSize: 17,fontFamily: "Oswald"),), // create field name
+                      const SizedBox(height: 5,), // add space between name and field
+                      _buildRiskInputField("input age between 20 and 80", isValidAge,"Age should be between 20 and 80",TextInputType.number,_ageCVDController), // create input field
 
-                  const Text("Gender :",style: TextStyle(fontSize: 17,),),
-                  const SizedBox(height: 5,),
-                  _buildNameField("biological male/ female", isValidGender,"Enter male or female",TextInputType.text),
+                      const SizedBox(height: 10,),
 
-
-                  const SizedBox(height: 10,),
-
-                  const Text("Height :",style: TextStyle(fontSize: 17,),),
-                  const SizedBox(height: 5,),
-                  _buildNameField("insert in centimeters", isValidHeight,"Enter valid height",TextInputType.number),
+                      const Text("Gender :",style: TextStyle(fontSize: 17,),), // create field name
+                      const SizedBox(height: 5,), // add space between name and field
+                      _buildRiskInputField("biological male/ female", isValidGender,"Enter male or female",TextInputType.text,_genderCVDController), // create function // create input field
 
 
-                  const SizedBox(height: 10,),
+                      const SizedBox(height: 10,),
 
-                  const Text("Weight :",style: TextStyle(fontSize: 17,),),
-                  const SizedBox(height: 5,),
-                  _buildNameField("insert in Kilograms", isValidHeight,"Enter valid weight",TextInputType.number),
+                      const Text("Height :",style: TextStyle(fontSize: 17,),), // create field name
+                      const SizedBox(height: 5,), // add space between name and field
+                      _buildRiskInputField("insert in centimeters", isValidHeight,"Enter valid height",TextInputType.number,_heightCVDController), // create input field
 
 
-        
-                  ElevatedButton(
-                    onPressed: (){
-                      if(_formKey.currentState!.validate()){ // check validity of foam filed
-                      print(name);
-                      }
-                      },
-                     child: const Text("Save"),)
-                ],
-              ),
-            ),
+                      const SizedBox(height: 10,),
+
+                      const Text("Weight :",style: TextStyle(fontSize: 17,),), // create field name
+                      const SizedBox(height: 5,), // add space between name and field
+                      _buildRiskInputField("insert in Kilograms", isValidHeight,"Enter valid weight",TextInputType.number,_weightCVDController), // create input field
+
+
+
+                      ElevatedButton(
+                        onPressed: (){
+                          if(_formKey.currentState!.validate()){ // check validity of foam filed
+                          print(_ageCVDController.text);
+                          print(_genderCVDController.text);
+                          print(_heightCVDController.text);
+                          print(_weightCVDController.text);
+                          }
+                          },
+                         child: const Text("Save"),)
+                    ],
+                  ),
+                ),
           ),
 
-        
-        
-        
+
           ],
         ),
       ),
@@ -237,19 +228,17 @@ final GlobalKey <FormState> _formKey =GlobalKey();// create global key object fo
 
 bool isValidAge(String input) {
   try {
-        int age = int.parse(input);
-    
-    // Check if age is a positive integer within a reasonable range
-    return age > 40 && age <= 80;
+        int age = int.parse(input);  // check input is int or not
+        return age > 40 && age <= 80; // check reasonable range
   } catch (e) {
-    // Catch any exception that occurs when parsing the input as an integer
-    return false;
+
+    return false; // if there is any error ,return false
   }
 }
 
 bool isValidGender(String input) {
 
-  if (input.toLowerCase()=="female" || input.toLowerCase()=="male"){
+  if (input.toLowerCase()=="female" || input.toLowerCase()=="male"){ //  check male or female
     return true;
   }
   return false;
@@ -257,24 +246,20 @@ bool isValidGender(String input) {
 
 bool isValidHeight(String input) {
   try {
-        int height = int.parse(input);
-    
-    // Check if age is a positive integer within a reasonable range
-    return height > 40 && height <= 200;
+        int height = int.parse(input); // check input is int or not
+        return height > 40 && height <= 200; // check reasonable range
   } catch (e) {
-    // Catch any exception that occurs when parsing the input as an integer
-    return false;
+
+    return false; // if there is any error ,return false
   }
 }
 
 bool isValidWeight(String input) {
   try {
-        int weight = int.parse(input);
-    
-    // Check if age is a positive integer within a reasonable range
-    return weight > 40 && weight <= 200;
+        int weight = int.parse(input); // check input is int or not
+        return weight > 40 && weight <= 200; // check reasonable range
   } catch (e) {
-    // Catch any exception that occurs when parsing the input as an integer
-    return false;
+
+    return false; // if there is any error ,return false
   }
 }
