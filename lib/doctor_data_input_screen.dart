6e052ebe4efb1de28abffef5e08349c3905482e9
tasheera <131450540+tasheera@users.noreply.github.com>
2024-02-10 10:41:07 +0530

    add confirmation dialog box to clear patient data

--- lib/doctor_data_input_screen.dart
+++ lib/doctor_data_input_screen.dart
@@ -88,8 +88,8 @@ final GlobalKey <FormState> _formKey =GlobalKey();// create global key object fo
               width: iconSize*0.7,// adjust width of image using current screen width
               height: iconSize*0.7, // height of image
             ),
-              GestureDetector(
-                onTap: () {},
+              TextButton(
+                onPressed: () {},
                 child: Image.asset(
                 'assets/data_input_report_icon.png', //image file direct
                 width: iconSize*1.6, // adjust width of image using current screen width
@@ -201,7 +201,7 @@ final GlobalKey <FormState> _formKey =GlobalKey();// create global key object fo
 
                       const Text("Weight :",style: TextStyle(fontSize: 17,),), // create field name
                       const SizedBox(height: 5,), // add space between name and field
-                      _buildRiskInputField("insert in Kilograms", isValidHeight,"Enter valid weight",TextInputType.number,_weightCVDController), // create input field
+                      _buildRiskInputField("insert in Kilograms", isValidWeight,"Enter valid weight",TextInputType.number,_weightCVDController), // create input field
 
                       const SizedBox(height: 20,),
 
@@ -233,7 +233,7 @@ final GlobalKey <FormState> _formKey =GlobalKey();// create global key object fo
                             ),
                             onPressed: (){
                                                                      
-                              _riskCVDParameterClear();
+                              _patientDataClearConfirmBox(context); // call confirmation dialog box
                               
                               },
                              child: const Text("Clear", style: TextStyle(color: Colors.white,fontSize: 15),),
@@ -256,12 +256,12 @@ final GlobalKey <FormState> _formKey =GlobalKey();// create global key object fo
 
 
   void calculateRiskLevel(){
-    //
+
     if(_formKey.currentState!.validate()){ // check validity of foam filed
-    print(_ageCVDController.text);
-    print(_genderCVDController.text);
-    print(_heightCVDController.text);
-    print(_weightCVDController.text);
+      print(_ageCVDController.text);
+      print(_genderCVDController.text);
+      print(_heightCVDController.text);
+      print(_weightCVDController.text);
     }
   }
 
@@ -271,6 +271,57 @@ final GlobalKey <FormState> _formKey =GlobalKey();// create global key object fo
     _heightCVDController.clear();
     _weightCVDController.clear();
 }
+
+
+  void _patientDataClearConfirmBox(BuildContext context) { // create clear confirmation box
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: const Text("Do you want to clear all patient's data?",style: TextStyle(fontSize: 25),),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // close the confirmation box
+              },
+              child: Container(
+                                height: 30,
+                width: 40,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(10),
+                  
+                ),
+                child: const Center(
+                    child: Text('No', style: TextStyle(color: Colors.white))
+                )
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                _riskCVDParameterClear(); // clear patient's data
+                Navigator.of(context).pop(); // close the confirmation box
+              },
+              child: Container(
+                height: 30,
+                width: 40,
+                  decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(10),
+                  
+                ),
+                child: const Center(
+                    child: Text('Yes', style: TextStyle(color: Colors.white))
+                )
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
 }
 
 
