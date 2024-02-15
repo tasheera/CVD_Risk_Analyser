0870d3563e2587cfac669b8a1a444a4d990c286b
luminimunasinghe <133509121+luminimunasinghe@users.noreply.github.com>
2024-02-15 10:20:55 +0530

    first step

---
+++ lib/lab_base_cal.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+
+main() {
+  //lab base chart
+
+  String input_D = "."; // with diabets
+  String input_G = " ."; // about gender
+  String input_S = "."; // about smoke
+
+  var inputA = 1; //for age
+  var SBP = 1;
+  var chole = 1;
+
+  if (input_D == "diablets") {
+    if (input_G=="men"){
+      if (input_S=="smoke"){
+        
+
+
+      }else{
+        //non smoke
+      }
+
+
+
+    }else{
+      //women
+    }
+
+
+
+
+
+
+}else{
+  //without diabites
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
