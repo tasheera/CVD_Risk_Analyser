b43663d7d3ac323ddb9f4fbcedb97eb896c33984
tasheera <131450540+tasheera@users.noreply.github.com>
2024-02-17 23:08:34 +0530

    add app bar with user name

--- lib/screens/user_data_input_screen.dart
+++ lib/screens/user_data_input_screen.dart
@@ -11,12 +11,35 @@ class UserInputScreen extends StatefulWidget {
 class _UserInputScreenState extends State<UserInputScreen> {
   @override
   Widget build(BuildContext context) {
-        double iconSize = MediaQuery.of(context).size.width * 0.08; // return current screen width 
+    double iconSize = MediaQuery.of(context).size.width * 0.08; // return current screen width 
 
 
     return Scaffold(
-      
-            
+         
+      appBar: AppBar(
+        
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween, // distribute free space
+          children: [
+            Text('Hi ${widget.name}\nCheck you CVD risk below', style: TextStyle(fontSize: iconSize*0.5),),
+            Image.asset(
+              'assets/data_input_Vector_icon.png', //image file directory
+              width: iconSize*0.7,// adjust width of image using current screen width
+              height: iconSize*0.7, // height of image
+            ),
+              TextButton(
+                onPressed:(){},
+                child: Image.asset(
+                'assets/data_input_report_icon.png', //image file direct
+                width: iconSize*1.6, // adjust width of image using current screen width
+                height: iconSize*1.6, // height of image
+                            ),
+              ),
+          
+            ],
+          ),
+
+      ),
     );
   }
 }
