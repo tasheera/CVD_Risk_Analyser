b3f5e378b53ab3bbdc5bf633857ac8f7af97e7cd
Niduwara Chandeepa <126352378+Niduwara28@users.noreply.github.com>
2024-02-20 13:41:43 +0530

    login and use as guest button done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cvd_risk_analyser/HomePage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "CVD Risk Analyser",
+      home: HomePage(),
+    );
   }
 }
