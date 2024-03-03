5c3f40d66d5567a7c553e7739a84a37d9ba53bd0
tasheera <131450540+tasheera@users.noreply.github.com>
2024-03-03 18:36:11 +0530

    Add filed validations and link chart choose screen

--- lib/signup_screen.dart
+++ lib/signup_screen.dart
@@ -1,8 +1,7 @@
+import 'package:cvd_risk_analyser/screens/chart_choose_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
-
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
@@ -14,74 +13,93 @@ class SignupScreen extends StatefulWidget {
 class _SignupScreenState extends State<SignupScreen> {
 
   final GlobalKey <FormState> _formKey =GlobalKey();// create global key object form key validate
-
-  
   final TextEditingController _signupEmailController = TextEditingController(); // controller for email text field
   final TextEditingController _signupPasswordController = TextEditingController(); // controller for password text field
   final TextEditingController _signupCodeController = TextEditingController();// controller for code text field
 
-
   @override
   Widget build(BuildContext context) {
-
-        SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Sign Up", style: TextStyle(color: Colors.white),),
+        title: const Text("Sign Up", style: TextStyle(color: Colors.white),),
         backgroundColor: Colors.red,
       ),
-
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Form(
           key: _formKey, 
           child: Container(
-            decoration: BoxDecoration(
-              
-            ),
             margin:  const EdgeInsets.all(16),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Text("Email address", textAlign: TextAlign.left, style: TextStyle(fontSize: 18, color: Colors.blue),),
-                TextFormField(
+                 const Text("Email ", textAlign: TextAlign.left, style: TextStyle(fontSize: 18, color: Colors.blue),),
+                 TextFormField(
+                 
                   controller: _signupEmailController,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     hintText: "someone@email.com",
                     hintStyle: TextStyle(color: Colors.grey)
                     
                   ),
-                ),
+                  validator: (value){  // validate use input
+                    if (value == null || value.isEmpty){// check null or is empty
+                      return "Email can not be empty";
+                      }
+                    if (!(value.contains("@") && value.contains("."))){ // chekc email validity
+                        return "Enter valid email";
+                      }
+                      return null;
+                    },
+                 
+                                     ),
+                                     const SizedBox(height: 20),
 
-                const SizedBox(height: 20),
-
-                Text("Password", textAlign: TextAlign.left, style: TextStyle(fontSize: 18, color: Colors.blue),),
+                const Text("Password", textAlign: TextAlign.left, style: TextStyle(fontSize: 18, color: Colors.blue),),
                 TextFormField(
                   controller: _signupPasswordController,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     hintText: "password",
-                    hintStyle: TextStyle(color: Colors.grey)
-                    
+                    hintStyle: TextStyle(color: Colors.grey) 
                   ),
+
+                  validator: (value){  // validate use input
+                    if (value == null || value.isEmpty){// check null or is empty
+                      return "Password can not be empty";
+                      }
+                    if (value.length <6 ){ // chekc email validity
+                        return "Password should contanis minimum 6 characters";
+                      }
+                      return null;
+                    },
+
                 ),
 
                const SizedBox(height: 20),
 
-               Text("Code number", textAlign: TextAlign.left, style: TextStyle(fontSize: 18, color: Colors.blue),),
+               const Text("Code number", textAlign: TextAlign.left, style: TextStyle(fontSize: 18, color: Colors.blue),),
                 TextFormField(
                   controller: _signupCodeController,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     hintText: "8 digit number",
                     hintStyle: TextStyle(color: Colors.grey)
-                    
                   ),
+
+                  validator: (value){  // validate use input
+                    if (value == null || value.isEmpty){// check null or is empty
+                      return "Code can not be empty";
+                      }
+                    if (value!="123##456"){ // chekc email validity
+                        return "Invalid Code";
+                      }
+                      return null;
+                    },
+
                 ),
 
                 const SizedBox(height: 20,),
-
-
                    
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
@@ -92,21 +110,12 @@ class _SignupScreenState extends State<SignupScreen> {
                         backgroundColor: Colors.green,
                         
                       ),
-                      onPressed: (){
-                        authTest("emailtt@gmil.com", "123456");
-                        },
-                      child: Text("Sign Up", style: TextStyle(color: Colors.white,fontSize:15),),
+                      onPressed: (){authTest(_signupEmailController.text, _signupPasswordController.text);},
+                      child: const Text("Sign Up", style: TextStyle(color: Colors.white,fontSize:15),),
                       ),
-
-
-                                                 
-                  ],
+                 ],
                 ),
                 
-
-
-
-
               ],
             ),
           )
@@ -115,12 +124,31 @@ class _SignupScreenState extends State<SignupScreen> {
 
     );
   }
-}
 
 void authTest(String email, String password ) async{
-  await FirebaseAuth.instance.createUserWithEmailAndPassword(
-    email: email,
-    password: password,
-  );
+
+      if(_formKey.currentState!.validate()){
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      Navigator.of(context).push(MaterialPageRoute(builder:(_){ // move to chart choose screen
+         return const ChartChoose();
+         }));
+      _signupCodeController.clear();
+      _signupEmailController.clear();
+      _signupPasswordController.clear();
+
+    }
+
+  }
+
+
 }
+
+
+
+
+
 
