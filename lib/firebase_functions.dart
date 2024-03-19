91c80830c0104a477b5f3abb73bde74f431d6599
Insaf Ifthikar <ifthikar.20211019@iit.ac.lk>
2024-03-20 00:42:20 +0530

    mend

--- lib/firebase_functions.dart
+++ lib/firebase_functions.dart
@@ -1,11 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+// Function to upload lab chart data to Firestore
+Future<void> uploadLabchartData(
+  String age, String gender, String sbpp, String diabetic, String cholesterol, String smoke, String cvdLevel) async {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-void uploadLabchartData( String age, String gender, String SBPP, String diabetic, String cholesterol,  String smoke , String cvdLevel ){
-  //TODO firebase data upload
-   print ("LabChart data upload, CVD level =  $cvdLevel");
+  // Upload data to Firestore
+  await firestore.collection('labchart_data').add({
+    'email': "test@gmail.com",
+    'age': age,
+    'gender': gender,
+    'SBPP': sbpp,
+    'diabetic': diabetic,
+    'cholesterol': cholesterol,
+    'smoke': smoke,
+    'cvdLevel': cvdLevel,
+  });
+  print ("LabChart data upload, CVD level =  $cvdLevel");
 }
 
-void uploadNonLabchartData( String age, String gender, String height, String weight, String SBPP, String smoke, String cvdLevel){
- //TODO firebase data upload
- print ("NonLabChart data upload,  CVD level =  $cvdLevel");
+// Function to upload non-lab chart data to Firestore
+Future<void> uploadNonLabchartData(
+    String age, String gender, String height, String weight, String sbpp, String smoke, String cvdLevel) async {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  // Upload data to Firestore
+  await firestore.collection('non_labchart_data').add({
+    'email': "test@gmail.com",
+    'age': age,
+    'gender': gender,
+    'height': height,
+    'weight': weight,
+    'SBPP': sbpp,
+    'smoke': smoke,
+    'cvdLevel': cvdLevel,
+  });
+  print ("NonLabChart data upload,  CVD level =  $cvdLevel");
 }
