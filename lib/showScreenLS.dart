6a11d6b6855741e4625926a5cac74eac43c5d1c0
luminimunasinghe <133509121+luminimunasinghe@users.noreply.github.com>
2024-03-22 02:14:03 +0530

    showScreen

--- lib/showScreenLS.dart
+++ lib/showScreenLS.dart
@@ -1,1 +1,64 @@
 import "package:flutter/material.dart";
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "ShowsScreen",
+      home: ShowScreen(),
+    );
+  }
+}
+
+class ShowScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Paitent History"),
+        ),
+        body: Container(
+          child: ListView(
+            // this have to auto genarate
+            scrollDirection: Axis.vertical, //scroll down
+            children: <Widget>[
+/**
+ * this part have to be itarare
+ * using colour,date and the display msg
+ */
+
+              ListTile(
+                leading: Icon(Icons.ac_unit_rounded),
+                title: Text("date  //calculate date"),
+                subtitle: Text("//display msg"),
+                trailing: Container(
+                  width: 2,
+                  height: 2,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.blue // colouer which output
+                      ),
+                  //onTop(){}, --> something wanna happen
+                ),
+              )
+
+              // Divider(),
+
+              // ListTile(
+              //   leading: Icon(Icons.ac_unit_rounded),
+              //   title:Text("date  //calculate date"),
+              //   subtitle:Text("//display msg"),
+              //   trailing:Container(
+              //     width: 2,
+              //     height: 2,
+              //     decoration: BoxDecoration(
+              //     shape: BoxShape.circle,
+              //     color:Colors.blue // colouer which output
+              //   ),
+              //   ),
+              // )
+            ],
+          ),
+        ));
+  }
+}
