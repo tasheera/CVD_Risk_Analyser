0d72f6023d14f9d8d994548d6253ed34fe51c562
tasheera <131450540+tasheera@users.noreply.github.com>
2024-03-23 18:45:53 +0530

    add snack bar
    add snack bar to show errors to user while signup

--- lib/signup_screen.dart
+++ lib/signup_screen.dart
@@ -25,101 +25,103 @@ class _SignupScreenState extends State<SignupScreen> {
         title: const Text("Sign Up", style: TextStyle(color: Colors.white),),
         backgroundColor: Colors.red,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Form(
-          key: _formKey, 
-          child: Container(
-            margin:  const EdgeInsets.all(16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                 const Text("Email ", textAlign: TextAlign.left, style: TextStyle(fontSize: 18, color: Colors.blue),),
-                 TextFormField(
-                 
-                  controller: _signupEmailController,
-                  decoration: const InputDecoration(
-                    hintText: "someone@email.com",
-                    hintStyle: TextStyle(color: Colors.grey)
-                    
-                  ),
-                  validator: (value){  // validate use input
-                    if (value == null || value.isEmpty){// check null or is empty
-                      return "Email can not be empty";
-                      }
-                    if (!(value.contains("@") && value.contains("."))){ // chekc email validity
-                        return "Enter valid email";
-                      }
-                      return null;
-                    },
-                 
-                                     ),
-                                     const SizedBox(height: 20),
-
-                const Text("Password", textAlign: TextAlign.left, style: TextStyle(fontSize: 18, color: Colors.blue),),
-                TextFormField(
-                  controller: _signupPasswordController,
-                  decoration: const InputDecoration(
-                    hintText: "password",
-                    hintStyle: TextStyle(color: Colors.grey) 
-                  ),
-
-                  validator: (value){  // validate use input
-                    if (value == null || value.isEmpty){// check null or is empty
-                      return "Password can not be empty";
-                      }
-                    if (value.length <6 ){ // chekc email validity
-                        return "Password should contanis minimum 6 characters";
-                      }
-                      return null;
-                    },
-
-                ),
-
-               const SizedBox(height: 20),
-
-               const Text("Code number", textAlign: TextAlign.left, style: TextStyle(fontSize: 18, color: Colors.blue),),
-                TextFormField(
-                  controller: _signupCodeController,
-                  decoration: const InputDecoration(
-                    hintText: "8 digit number",
-                    hintStyle: TextStyle(color: Colors.grey)
-                  ),
-
-                  validator: (value){  // validate use input
-                    if (value == null || value.isEmpty){// check null or is empty
-                      return "Code can not be empty";
-                      }
-                    if (value!="123##456"){ // chekc email validity
-                        return "Invalid Code";
-                      }
-                      return null;
-                    },
-
-                ),
-
-                const SizedBox(height: 20,),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(
+            key: _formKey, 
+            child: Container(
+              margin:  const EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                   const Text("Email ", textAlign: TextAlign.left, style: TextStyle(fontSize: 18, color: Colors.blue),),
+                   TextFormField(
                    
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                           
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.green,
-                        
-                      ),
-                      onPressed: (){authTest(_signupEmailController.text, _signupPasswordController.text);},
-                      child: const Text("Sign Up", style: TextStyle(color: Colors.white,fontSize:15),),
-                      ),
-                 ],
-                ),
-                
-              ],
-            ),
+                    controller: _signupEmailController,
+                    decoration: const InputDecoration(
+                      hintText: "someone@email.com",
+                      hintStyle: TextStyle(color: Colors.grey)
+                      
+                    ),
+                    validator: (value){  // validate use input
+                      if (value == null || value.isEmpty){// check null or is empty
+                        return "Email can not be empty";
+                        }
+                      if (!(value.contains("@") && value.contains("."))){ // chekc email validity
+                          return "Enter valid email";
+                        }
+                        return null;
+                      },
+                   
+                                       ),
+                                       const SizedBox(height: 20),
+        
+                  const Text("Password", textAlign: TextAlign.left, style: TextStyle(fontSize: 18, color: Colors.blue),),
+                  TextFormField(
+                    controller: _signupPasswordController,
+                    decoration: const InputDecoration(
+                      hintText: "password",
+                      hintStyle: TextStyle(color: Colors.grey) 
+                    ),
+        
+                    validator: (value){  // validate use input
+                      if (value == null || value.isEmpty){// check null or is empty
+                        return "Password can not be empty";
+                        }
+                      if (value.length <6 ){ // chekc email validity
+                          return "Password should contanis minimum 6 characters";
+                        }
+                        return null;
+                      },
+        
+                  ),
+        
+                 const SizedBox(height: 20),
+        
+                 const Text("Code number", textAlign: TextAlign.left, style: TextStyle(fontSize: 18, color: Colors.blue),),
+                  TextFormField(
+                    controller: _signupCodeController,
+                    decoration: const InputDecoration(
+                      hintText: "8 digit number",
+                      hintStyle: TextStyle(color: Colors.grey)
+                    ),
+        
+                    validator: (value){  // validate use input
+                      if (value == null || value.isEmpty){// check null or is empty
+                        return "Code can not be empty";
+                        }
+                      if (value!="123##456"){ // chekc email validity
+                          return "Invalid Code";
+                        }
+                        return null;
+                      },
+        
+                  ),
+        
+                  const SizedBox(height: 20,),
+                     
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                             
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green,
+                          
+                        ),
+                        onPressed: (){authTest(_signupEmailController.text, _signupPasswordController.text);},
+                        child: const Text("Sign Up", style: TextStyle(color: Colors.white,fontSize:15),),
+                        ),
+                   ],
+                  ),
+                  
+                ],
+              ),
+            )
           )
-        )
+        ),
       ),
 
     );
@@ -127,7 +129,8 @@ class _SignupScreenState extends State<SignupScreen> {
 
 void authTest(String email, String password ) async{
 
-      if(_formKey.currentState!.validate()){
+  try {
+     if(_formKey.currentState!.validate()){
       await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: email,
         password: password,
@@ -140,11 +143,35 @@ void authTest(String email, String password ) async{
       _signupEmailController.clear();
       _signupPasswordController.clear();
 
+      
+
     }
+  }on Exception catch (e){
+    print(e);
+    _showSnackbar(context,e.toString());
+  }
+
+ 
 
   }
 
+    void _showSnackbar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        duration: Duration(seconds: 3), // Adjust the duration as needed
+        action: SnackBarAction(
+          label: 'Close',
+          onPressed: () {
+            // Some action to be performed when "Close" is pressed
+          },
+        ),
+      ),
+    );
 
+
+
+}
 }
 
 
